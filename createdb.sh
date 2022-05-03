ad5dc13e6f03e488c33fbe3b5d483fb6e4e1afb0
Igor Bubelov <igor@bubelov.com>
2022-05-03 13:48:07 +0700

    Handle more edge cases

--- createdb.sh
+++ createdb.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if ! command -v sqlite3 &> /dev/null
+then
+    echo "sqlite3 is not installed"
+    exit
+fi
+
 sqlite3 btcmap.db < schema.sql
 
-echo 'Created btcmap.db'
+if [ $? -eq 0 ]; then
+  echo 'Created btcmap.db'
+fi
+
